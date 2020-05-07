43661a2238bc669094629c04eb922c57d572482f
Archisman Tripathi <36406969+archismantripathi@users.noreply.github.com>
2020-05-07 09:35:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+var randomNumber1=(Math.ceil(Math.random()*6));
+var randomNumber2=(Math.ceil(Math.random()*6));
+
+document.querySelector(".img1").setAttribute("src","images/dice"+randomNumber1+".png");
+document.querySelector(".img2").setAttribute("src","images/dice"+randomNumber2+".png");
+
+if(randomNumber1 === randomNumber2)
+{
+  document.querySelector("h1").innerHTML="🎲 It's a Draw! 🎲";
+}
+else if (randomNumber1>2)
+{
+  document.querySelector("h1").innerHTML="⛳ Player 1 is Winner 🎲";
+}
+else
+{
+  document.querySelector("h1").innerHTML="🎲 Player 2 is Winner ⛳";
+}
